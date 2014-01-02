8ed4156e7fe0932795d810ef03bf9d9b9c9496bf
dannvertigo <dann@vertigo.com>
2014-01-02 12:05:48 -0600

    added logic to deferred tasks to update read models.

--- SampleAppEngine/src/com/simplecqrs/appengine/example/handlers/HandleAttendeeNameChangedTask.java
+++ SampleAppEngine/src/com/simplecqrs/appengine/example/handlers/HandleAttendeeNameChangedTask.java
@@ -1,5 +1,12 @@
 package com.simplecqrs.appengine.example.handlers;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Transaction;
 import com.google.appengine.api.taskqueue.DeferredTask;
 import com.simplecqrs.appengine.example.domain.AttendeeNameChanged;
 import com.simplecqrs.appengine.messaging.MessageLog;
@@ -19,6 +26,25 @@ public class HandleAttendeeNameChangedTask implements DeferredTask{
 	
 	@Override
 	public void run() {
-		MessageLog.log(String.format("Attendee %s name changed. Update your read models.", event.getAttendeeId().toString()));
+		
+		DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
+        Transaction transaction = datastoreService.beginTransaction();
+		
+        Key attendeeKey = KeyFactory.createKey("Attendee", event.getAttendeeId().toString());
+        Entity attendee = null;
+        
+        try {
+                attendee = datastoreService.get(attendeeKey);
+                attendee.setProperty("FirstName", event.getFirstName());
+                attendee.setProperty("LastName", event.getLastName());
+                datastoreService.put(attendee);
+                transaction.commit();
+                
+        } catch (EntityNotFoundException e) {
+        	MessageLog.log(e);
+        } finally {
+        	if(transaction != null && transaction.isActive())
+                transaction.rollback();
+        }
 	}
 }
--- SampleAppEngine/src/com/simplecqrs/appengine/example/handlers/HandleAttendeeRegisteredTask.java
+++ SampleAppEngine/src/com/simplecqrs/appengine/example/handlers/HandleAttendeeRegisteredTask.java
@@ -1,8 +1,14 @@
 package com.simplecqrs.appengine.example.handlers;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Transaction;
 import com.google.appengine.api.taskqueue.DeferredTask;
 import com.simplecqrs.appengine.example.domain.AttendeeRegistered;
-import com.simplecqrs.appengine.messaging.MessageLog;
 
 /**
  * Deferred task to handle an attendee registering
@@ -19,6 +25,28 @@ public class HandleAttendeeRegisteredTask implements DeferredTask{
 	
 	@Override
 	public void run() {
-		MessageLog.log(String.format("Attendee %s registered. Update your read models.", event.getAttendeeId().toString()));
+		DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
+        Transaction transaction = datastoreService.beginTransaction();
+		
+        Key attendeeKey = KeyFactory.createKey("Attendee", event.getAttendeeId().toString());
+        Entity attendee = null;
+        
+        try {
+                attendee = datastoreService.get(attendeeKey);
+                
+                //do nothing if found. This is a new registration
+                
+        } catch (EntityNotFoundException e) {
+        	
+        	attendee = new Entity(attendeeKey);
+        	attendee.setProperty("FirstName", event.getFirstName());
+            attendee.setProperty("LastName", event.getLastName());
+            datastoreService.put(attendee);
+            transaction.commit();
+            
+        } finally {
+        	if(transaction != null && transaction.isActive())
+                transaction.rollback();
+        }
 	}
 }
